24c25359c04e71e7d1ea5f415a52daeab8d1ce7e
ershovda-1 <ershovda12@gmail.com>
2024-03-05 11:37:44 +0300

    com1

--- number one.py
+++ number one.py
@@ -1,5 +1,33 @@
-f = open("likestudents.txt")
+class Student:
+    id_ = 0
+    name = ""
+    p_id = 0
+    cls = ""
+    mark = 0
+
+
+f = open("students.csv", encoding="UTF8")
 a = []
+stud = []
 for i in f:
-    print(1)
+    a.append(i[:-1])
 f.close()
+s, k = [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]
+for i in range(1, len(a)):
+    stud.append(Student())
+    stud[i - 1].id_ = int(a[i].split(",")[0])
+    stud[i - 1].name = a[i].split(",")[1]
+    stud[i - 1].p_id = int(a[i].split(",")[2])
+    stud[i - 1].cls = a[i].split(",")[3]
+    m = a[i].split(",")[4]
+    if m.isdigit():
+        stud[i - 1].mark = int(m)
+        s[int(stud[i - 1].cls[:-1]) - 7] += int(m)
+        k[int(stud[i - 1].cls[:-1]) - 7] += 1
+for i in stud:
+    if not i.mark:
+        i.mark = int((s[int(i.cls[:-1]) - 7] / k[int(i.cls[:-1]) - 7]) * 1000) / 1000
+name = input()
+for i in stud:
+    if name in i.name:
+        print(f"Ты получил: {i.mark}, за проект - {i.p_id}")
